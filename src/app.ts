17450b08ffa1f26e3e4fe280057bc2b43d24e4b5
rakib <rakibshahid1298@gmail.com>
2025-04-01 03:48:32 +0000

    removed rate limit

--- src/app.ts
+++ src/app.ts
@@ -6,235 +6,239 @@ import { FetchUserDataRequest } from './types';
 import apicache from 'apicache';
 import axios from 'axios';
 import {
-  userContestRankingInfoQuery,
-  discussCommentsQuery,
-  discussTopicQuery,
-  userProfileUserQuestionProgressV2Query,
-  skillStatsQuery,
-  getUserProfileQuery,
-  userProfileCalendarQuery,
-  officialSolutionQuery,
-  dailyQeustion,
+    userContestRankingInfoQuery,
+    discussCommentsQuery,
+    discussTopicQuery,
+    userProfileUserQuestionProgressV2Query,
+    skillStatsQuery,
+    getUserProfileQuery,
+    userProfileCalendarQuery,
+    officialSolutionQuery,
+    dailyQeustion,
 } from './GQLQueries/newQueries';
 
 const app = express();
+app.set('trust proxy', 1);
 let cache = apicache.middleware;
 const API_URL = process.env.LEETCODE_API_URL || 'https://leetcode.com/graphql';
 
 const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour
-  limit: 60,
-  standardHeaders: 'draft-7',
-  legacyHeaders: false,
-  message: 'Too many request from this IP, try again in 1 hour',
+    windowMs: 60 * 60 * 1000, // 1 hour
+    limit: 3600,
+    standardHeaders: 'draft-7',
+    legacyHeaders: false,
+    message: 'Too many request from this IP, try again in 1 hour',
 });
 
 app.use(cache('5 minutes'));
-app.use(cors()); //enable all CORS request
+app.use(cors({
+    origin: 'https://leetcode.server.rakibshahid.com', // Restrict to your domain
+    credentials: true // Allow cookies
+  }));
 app.use(limiter); //limit to all API
 app.use((req: express.Request, _res: Response, next: NextFunction) => {
-  console.log('Requested URL:', req.originalUrl);
-  next();
+    console.log('Requested URL:', req.originalUrl);
+    next();
 });
 
 async function queryLeetCodeAPI(query: string, variables: any) {
-  try {
-    const response = await axios.post(API_URL, { query, variables });
-    if (response.data.errors) {
-      throw new Error(response.data.errors[0].message);
+    try {
+        const response = await axios.post(API_URL, { query, variables });
+        if (response.data.errors) {
+            throw new Error(response.data.errors[0].message);
+        }
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            throw new Error(`Error from LeetCode API: ${error.response.data}`);
+        } else if (error.request) {
+            throw new Error('No response received from LeetCode API');
+        } else {
+            throw new Error(`Error in setting up the request: ${error.message}`);
+        }
     }
-    return response.data;
-  } catch (error) {
-    if (error.response) {
-      throw new Error(`Error from LeetCode API: ${error.response.data}`);
-    } else if (error.request) {
-      throw new Error('No response received from LeetCode API');
-    } else {
-      throw new Error(`Error in setting up the request: ${error.message}`);
-    }
-  }
 }
 
 app.get('/', (_req, res) => {
-  res.json({
-    apiOverview:
-      'Welcome to the Alfa-Leetcode-API! Alfa-Leetcode-Api is a custom solution born out of the need for a well-documented and detailed LeetCode API. This project is designed to provide developers with endpoints that offer insights into a user"s profile, badges, solved questions, contest details, contest history, submissions, and also daily questions, selected problem, list of problems.',
-    apiEndpointsLink:
-      'https://github.com/alfaarghya/alfa-leetcode-api?tab=readme-ov-file#endpoints-',
-    routes: {
-      userDetails: {
-        description:
-          'Endpoints for retrieving detailed user profile information on Leetcode.',
-        Method: 'GET',
-        '/:username': 'Get your leetcodevis profile Details',
-        '/:username/badges': 'Get your badges',
-        '/:username/solved': 'Get total number of question you solved',
-        '/:username/contest': 'Get your contest details',
-        '/:username/contest/history': 'Get all contest history',
-        '/:username/submission': 'Get your last 20 submission',
-        '/:username/acSubmission': 'Get your last 20 accepted submission',
-        '/:username/calendar': 'Get your submission calendar',
-        '/userProfile/:username': 'Get full profile details in one call',
-        '/userProfileCalendar?username=yourname&year=2024':
-          'Get your calendar details with year',
-        '/languageStats?username=yourname': 'Get the language stats of a user',
-        '/userProfileUserQuestionProgressV2/:userSlug':
-          'Get your question progress',
-        '/skillStats/:username': 'Get your skill stats',
-      },
-      contest: {
-        description:
-          'Endpoints for retrieving contest ranking and performance data.',
-        Method: 'GET',
-        '/userContestRankingInfo/:username': 'Get user contest ranking info',
-      },
-      discussion: {
-        description: 'Endpoints for fetching discussion topics and comments.',
-        Method: 'GET',
-        '/trendingDiscuss?first=20': 'Get top 20 trending discussions',
-        '/discussTopic/:topicId': 'Get discussion topic',
-        '/discussComments/:topicId': 'Get discussion comments',
-      },
-      problems: {
-        description:
-          'Endpoints for fetching problem-related data, including lists, details, and solutions.',
-        Method: 'GET',
-        singleProblem: {
-          '/select?titleSlug=two-sum': 'Get selected Problem',
-          '/daily': 'Get daily Problem',
-          '/dailyQuestion': 'Get raw daily question',
+    res.json({
+        apiOverview:
+            'Welcome to the Alfa-Leetcode-API! Alfa-Leetcode-Api is a custom solution born out of the need for a well-documented and detailed LeetCode API. This project is designed to provide developers with endpoints that offer insights into a user"s profile, badges, solved questions, contest details, contest history, submissions, and also daily questions, selected problem, list of problems.',
+        apiEndpointsLink:
+            'https://github.com/alfaarghya/alfa-leetcode-api?tab=readme-ov-file#endpoints-',
+        routes: {
+            userDetails: {
+                description:
+                    'Endpoints for retrieving detailed user profile information on Leetcode.',
+                Method: 'GET',
+                '/:username': 'Get your leetcodevis profile Details',
+                '/:username/badges': 'Get your badges',
+                '/:username/solved': 'Get total number of question you solved',
+                '/:username/contest': 'Get your contest details',
+                '/:username/contest/history': 'Get all contest history',
+                '/:username/submission': 'Get your last 20 submission',
+                '/:username/acSubmission': 'Get your last 20 accepted submission',
+                '/:username/calendar': 'Get your submission calendar',
+                '/userProfile/:username': 'Get full profile details in one call',
+                '/userProfileCalendar?username=yourname&year=2024':
+                    'Get your calendar details with year',
+                '/languageStats?username=yourname': 'Get the language stats of a user',
+                '/userProfileUserQuestionProgressV2/:userSlug':
+                    'Get your question progress',
+                '/skillStats/:username': 'Get your skill stats',
+            },
+            contest: {
+                description:
+                    'Endpoints for retrieving contest ranking and performance data.',
+                Method: 'GET',
+                '/userContestRankingInfo/:username': 'Get user contest ranking info',
+            },
+            discussion: {
+                description: 'Endpoints for fetching discussion topics and comments.',
+                Method: 'GET',
+                '/trendingDiscuss?first=20': 'Get top 20 trending discussions',
+                '/discussTopic/:topicId': 'Get discussion topic',
+                '/discussComments/:topicId': 'Get discussion comments',
+            },
+            problems: {
+                description:
+                    'Endpoints for fetching problem-related data, including lists, details, and solutions.',
+                Method: 'GET',
+                singleProblem: {
+                    '/select?titleSlug=two-sum': 'Get selected Problem',
+                    '/daily': 'Get daily Problem',
+                    '/dailyQuestion': 'Get raw daily question',
+                },
+                problemList: {
+                    '/problems': 'Get list of 20 problems',
+                    '/problems?limit=50': 'Get list of some problems',
+                    '/problems?tags=array+math': 'Get list problems on selected topics',
+                    '/problems?tags=array+math+string&limit=5':
+                        'Get list some problems on selected topics',
+                    '/officialSolution?titleSlug=two-sum':
+                        'Get official solution of selected problem',
+                },
+            },
         },
-        problemList: {
-          '/problems': 'Get list of 20 problems',
-          '/problems?limit=50': 'Get list of some problems',
-          '/problems?tags=array+math': 'Get list problems on selected topics',
-          '/problems?tags=array+math+string&limit=5':
-            'Get list some problems on selected topics',
-          '/officialSolution?titleSlug=two-sum':
-            'Get official solution of selected problem',
-        },
-      },
-    },
-  });
+    });
 });
 
 app.get('/officialSolution', async (req, res) => {
-  const { titleSlug } = req.query;
+    const { titleSlug } = req.query;
 
-  if (!titleSlug) {
-    return res.status(400).json({ error: 'Missing titleSlug query parameter' });
-  }
-  try {
-    const data = await queryLeetCodeAPI(officialSolutionQuery, { titleSlug });
-    return res.json(data);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
+    if (!titleSlug) {
+        return res.status(400).json({ error: 'Missing titleSlug query parameter' });
+    }
+    try {
+        const data = await queryLeetCodeAPI(officialSolutionQuery, { titleSlug });
+        return res.json(data);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 });
 
 app.get('/userProfileCalendar', async (req, res) => {
-  const { username, year } = req.query;
+    const { username, year } = req.query;
 
-  if (!username || !year || typeof year !== 'string') {
-    return res
-      .status(400)
-      .json({ error: 'Missing or invalid username or year query parameter' });
-  }
+    if (!username || !year || typeof year !== 'string') {
+        return res
+            .status(400)
+            .json({ error: 'Missing or invalid username or year query parameter' });
+    }
 
-  try {
-    const data = await queryLeetCodeAPI(userProfileCalendarQuery, {
-      username,
-      year: parseInt(year),
-    });
-    return res.json(data);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
+    try {
+        const data = await queryLeetCodeAPI(userProfileCalendarQuery, {
+            username,
+            year: parseInt(year),
+        });
+        return res.json(data);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 });
 
 // Format data
 const formatData = (data: any) => {
-  return {
-    totalSolved: data.matchedUser.submitStats.acSubmissionNum[0].count,
-    totalSubmissions: data.matchedUser.submitStats.totalSubmissionNum,
-    totalQuestions: data.allQuestionsCount[0].count,
-    easySolved: data.matchedUser.submitStats.acSubmissionNum[1].count,
-    totalEasy: data.allQuestionsCount[1].count,
-    mediumSolved: data.matchedUser.submitStats.acSubmissionNum[2].count,
-    totalMedium: data.allQuestionsCount[2].count,
-    hardSolved: data.matchedUser.submitStats.acSubmissionNum[3].count,
-    totalHard: data.allQuestionsCount[3].count,
-    ranking: data.matchedUser.profile.ranking,
-    contributionPoint: data.matchedUser.contributions.points,
-    reputation: data.matchedUser.profile.reputation,
-    submissionCalendar: JSON.parse(data.matchedUser.submissionCalendar),
-    recentSubmissions: data.recentSubmissionList,
-    matchedUserStats: data.matchedUser.submitStats,
-  };
+    return {
+        totalSolved: data.matchedUser.submitStats.acSubmissionNum[0].count,
+        totalSubmissions: data.matchedUser.submitStats.totalSubmissionNum,
+        totalQuestions: data.allQuestionsCount[0].count,
+        easySolved: data.matchedUser.submitStats.acSubmissionNum[1].count,
+        totalEasy: data.allQuestionsCount[1].count,
+        mediumSolved: data.matchedUser.submitStats.acSubmissionNum[2].count,
+        totalMedium: data.allQuestionsCount[2].count,
+        hardSolved: data.matchedUser.submitStats.acSubmissionNum[3].count,
+        totalHard: data.allQuestionsCount[3].count,
+        ranking: data.matchedUser.profile.ranking,
+        contributionPoint: data.matchedUser.contributions.points,
+        reputation: data.matchedUser.profile.reputation,
+        submissionCalendar: JSON.parse(data.matchedUser.submissionCalendar),
+        recentSubmissions: data.recentSubmissionList,
+        matchedUserStats: data.matchedUser.submitStats,
+    };
 };
 
 app.get('/userProfile/:id', async (req, res) => {
-  const user = req.params.id;
+    const user = req.params.id;
 
-  try {
-    const data = await queryLeetCodeAPI(getUserProfileQuery, {
-      username: user,
-    });
-    if (data.errors) {
-      res.send(data);
-    } else {
-      res.send(formatData(data.data));
+    try {
+        const data = await queryLeetCodeAPI(getUserProfileQuery, {
+            username: user,
+        });
+        if (data.errors) {
+            res.send(data);
+        } else {
+            res.send(formatData(data.data));
+        }
+    } catch (error) {
+        res.send(error);
     }
-  } catch (error) {
-    res.send(error);
-  }
 });
 
 const handleRequest = async (res: Response, query: string, params: any) => {
-  try {
-    const data = await queryLeetCodeAPI(query, params);
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+    try {
+        const data = await queryLeetCodeAPI(query, params);
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 app.get('/dailyQuestion', (_, res) => {
-  handleRequest(res, dailyQeustion, {});
+    handleRequest(res, dailyQeustion, {});
 });
 
 app.get('/skillStats/:username', (req, res) => {
-  const { username } = req.params;
-  handleRequest(res, skillStatsQuery, { username });
+    const { username } = req.params;
+    handleRequest(res, skillStatsQuery, { username });
 });
 
 app.get('/userProfileUserQuestionProgressV2/:userSlug', (req, res) => {
-  const { userSlug } = req.params;
-  handleRequest(res, userProfileUserQuestionProgressV2Query, { userSlug });
+    const { userSlug } = req.params;
+    handleRequest(res, userProfileUserQuestionProgressV2Query, { userSlug });
 });
 
 app.get('/discussTopic/:topicId', (req, res) => {
-  const topicId = parseInt(req.params.topicId);
-  handleRequest(res, discussTopicQuery, { topicId });
+    const topicId = parseInt(req.params.topicId);
+    handleRequest(res, discussTopicQuery, { topicId });
 });
 
 app.get('/discussComments/:topicId', (req, res) => {
-  const topicId = parseInt(req.params.topicId);
-  const {
-    orderBy = 'newest_to_oldest',
-    pageNo = 1,
-    numPerPage = 10,
-  } = req.query;
-  handleRequest(res, discussCommentsQuery, {
-    topicId,
-    orderBy,
-    pageNo,
-    numPerPage,
-  });
+    const topicId = parseInt(req.params.topicId);
+    const {
+        orderBy = 'newest_to_oldest',
+        pageNo = 1,
+        numPerPage = 10,
+    } = req.query;
+    handleRequest(res, discussCommentsQuery, {
+        topicId,
+        orderBy,
+        pageNo,
+        numPerPage,
+    });
 });
 
 app.get('/userContestRankingInfo/:username', (req, res) => {
-  const { username } = req.params;
-  handleRequest(res, userContestRankingInfoQuery, { username });
+    const { username } = req.params;
+    handleRequest(res, userContestRankingInfoQuery, { username });
 });
 
 //get the daily leetCode problem
@@ -253,14 +257,14 @@ app.get('/languageStats', leetcode.languageStats);
 
 // Construct options object on all user routes.
 app.use(
-  '/:username*',
-  (req: FetchUserDataRequest, _res: Response, next: NextFunction) => {
-    req.body = {
-      username: req.params.username,
-      limit: req.query.limit,
-    };
-    next();
-  }
+    '/:username*',
+    (req: FetchUserDataRequest, _res: Response, next: NextFunction) => {
+        req.body = {
+            username: req.params.username,
+            limit: req.query.limit,
+        };
+        next();
+    }
 );
 
 //get user profile details
